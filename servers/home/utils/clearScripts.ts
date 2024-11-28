f8d39e44524a23bb7f8ac40b3101b416cf07c6aa
gr8pefish <gr8pefish@gmail.com>
2024-11-28 07:57:17 -0800

    Only clear non .exe scripts

--- servers/home/utils/clearScripts.ts
+++ servers/home/utils/clearScripts.ts
@@ -2,7 +2,7 @@ export async function main(ns: NS) {
     ns.tprint('Deleting all scripts on server {ns.getServer().hostname}')
     let scripts = ns.ls(ns.getServer().hostname)
     scripts.forEach( s => {
-            ns.rm(s)
+            if (!s.endsWith('.exe')) ns.rm(s) //don't remove brutessh/etc
         }
     )
 }
