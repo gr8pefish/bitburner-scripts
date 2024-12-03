28639df005b9f6dd1ecb750c20aa955eee0ac44a
gr8pefish <gr8pefish@gmail.com>
2024-12-03 10:00:59 -0700

    Update protobatcher
    Some misc changes from all the dependencies

--- servers/home/hack/controllers/protobatcher.ts
+++ servers/home/hack/controllers/protobatcher.ts
@@ -1,78 +1,18 @@
-import { getBestTargetsNaiive, getHgwExecTimes } from '../hackUtils';
-import { HWGW_CONSTANTS, HWGW_TYPES } from '../../core/constants';
-import { HWGW_ThreadCounts, HWGW_StartEndTimes, HWGW_RamBlocks, HWGW_Job } from '../../core/datatypes';
-import { isPrepped, iterativePrep } from '../prep';
-import { disableLog, getFreeRAM } from '../../core/coreUtils';
+import { calculateRamUsage, getBestTargetsNaiive, getHgwExecTimes, getHWGWThreadCounts, getOptimalLeechPercent, getStartEndTimes } from '../hackUtils';
+import { HWGW_CONSTANTS, HWGW_TYPE, HWGW_TYPES } from '../../core/constants';
+import { HWGW_ThreadCounts, HWGW_StartEndTimes, HWGW_RamBlocks, HWGW_Job, ServerSubset, Job, Batch_Job } from '../../core/datatypes';
+import { isPrepped, iterativePrepImproved } from '../prep';
+import { disableLog, freeRamPredicate, getFreeRAM, RamNetwork } from '../../core/coreUtils';
 
-function getHWGWThreadCounts(ns: NS, target: string, leechPercent: number, print = false): HWGW_ThreadCounts {
-    // hack is simple, just get the leech amt, floor it
-    const hackThreads = Math.floor(ns.hackAnalyzeThreads(target, ns.getServerMaxMoney(target) * leechPercent));
-    
-    // weaken is easy as well, just account for hack increases, ceil just in case
-    const weaken1Threads = Math.ceil((hackThreads * HWGW_CONSTANTS.hack.SEC_CHANGE) / HWGW_CONSTANTS.weaken1.SEC_CHANGE);
-    
-    // grow is trickier - get multiplier from hackThreads vs maxMoney, and ceil it to ensure growth
-    const maxMoney = ns.getServerMaxMoney(target);
-    const multFactor = maxMoney / (maxMoney - (maxMoney * (ns.hackAnalyze(target) * hackThreads)));
-    const growThreads = Math.ceil(ns.growthAnalyze(target, multFactor)); //TODO: add extra to account for leveling?
-    
-    // weaken is easy as well, just account for grow increases, ceil just in case
-    const weaken2Threads = Math.ceil((growThreads * HWGW_CONSTANTS.grow.SEC_CHANGE) / HWGW_CONSTANTS.weaken2.SEC_CHANGE);
 
-    /** Old print statements */
-    // const moneyPrintFrom = ns.getServerMaxMoney(target) - (ns.getServerMaxMoney(target) * leechPercent);
-    // ns.print(`hackThreads: ${hackThreads} for ${ns.formatNumber(ns.getServerMaxMoney(target) * leechPercent)} (${ns.formatPercent(leechPercent)} leech, down to ${ns.formatNumber(moneyPrintFrom)})`);
-    // ns.print(`weaken1Threads: ${weaken1Threads} for ${(hackThreads * HGW_TYPE.HACK.SEC_INCR)} sec incr from hack (${ns.formatNumber(HGW_TYPE.WEAKEN.SEC_DECR)} decr ea)`);
-    // const moneyToPredicted = moneyPrintFrom * multFactor;
-    // ns.print(`growThreads: ${growThreads} for ${multFactor} multFactor, should be from ${ns.formatNumber(moneyPrintFrom)} to ${ns.formatNumber(moneyToPredicted)}(${ns.formatNumber(ns.getServerMaxMoney(target))})`);
-    // ns.print(`weaken2Threads: ${weaken2Threads} for ${(growThreads * HGW_TYPE.GROW.SEC_INCR)} sec incr from grow (${ns.formatNumber(HGW_TYPE.WEAKEN.SEC_DECR)} decr ea)`);
-    // ns.print(`\n`);
-
-    const threadCounts = new HWGW_ThreadCounts(hackThreads, weaken1Threads, growThreads, weaken2Threads);
-    if (print) threadCounts.print(ns);
-
-    return threadCounts;
-}
-
-function getStartEndTimes(ns: NS, target: string, print = false): HWGW_StartEndTimes {
-    let executionTimes = getHgwExecTimes(ns, target);
-    
-    const BUFFER_MS = 50;
-
-    let startEndTimes = new HWGW_StartEndTimes();
-
-    let longestTime = Math.max(executionTimes.HackTime, executionTimes.GrowTime, executionTimes.WeakenTime); //assuming weaken for now, will break if wrong
-    let fullTime = longestTime + (BUFFER_MS * 2); //weaken1 (0start) + (grow + weaken2 buffers)
-
-    //weaken2
-    startEndTimes.weaken2End = fullTime;
-    startEndTimes.weaken2Start = fullTime - executionTimes.WeakenTime;
-
-    //weaken1
-    startEndTimes.weaken1End = longestTime;
-    startEndTimes.weaken1Start = 0;
-
-    //hack
-    startEndTimes.hackEnd = fullTime - BUFFER_MS * 3;
-    startEndTimes.hackStart = startEndTimes.hackEnd - executionTimes.HackTime;
-
-    //grow
-    startEndTimes.growEnd = fullTime - BUFFER_MS;
-    startEndTimes.growStart = startEndTimes.growEnd - executionTimes.GrowTime;
-
-    //print
-    if (print) startEndTimes.print(ns);
-
-    return startEndTimes
-}
-
+//@deprecated
 // // returns -1 if not enough space in host
 function getTotalRamNeeded(ns: NS, threadCounts: HWGW_ThreadCounts, host?: string): number {
     let totalRamNeeded = 
-        threadCounts.growThreads * HWGW_CONSTANTS.grow.RAM_COST +
-        threadCounts.hackThreads * HWGW_CONSTANTS.hack.RAM_COST +
-        threadCounts.weaken1Threads * HWGW_CONSTANTS.weaken1.RAM_COST +
-        threadCounts.weaken2Threads * HWGW_CONSTANTS.weaken2.RAM_COST;
+        threadCounts.grow * HWGW_CONSTANTS.grow.RAM_COST +
+        threadCounts.hack * HWGW_CONSTANTS.hack.RAM_COST +
+        threadCounts.weaken1 * HWGW_CONSTANTS.weaken1.RAM_COST +
+        threadCounts.weaken2 * HWGW_CONSTANTS.weaken2.RAM_COST;
     
     if (host) {
         let freePercent = host == 'home' ? 0.1 : 0;
@@ -89,48 +29,68 @@ function getTotalRamNeeded(ns: NS, threadCounts: HWGW_ThreadCounts, host?: strin
 
 // Limited by RAM for grow, time for weaken
 //TODO: assumes home
-async function exploit(ns: NS, target: string, leechPercent: number, print = false) {
+// async function exploit(ns: NS, target: string, leechPercent: number, print = false) {
 
-    let threadCounts = getHWGWThreadCounts(ns, target, leechPercent, print);
-    let ramBlocks = new HWGW_RamBlocks(threadCounts);
+//     let threadCounts = getHWGWThreadCounts(ns, target, leechPercent, print);
+//     let ramBlocks = new HWGW_RamBlocks(threadCounts);
 
-    let ramCost = getTotalRamNeeded(ns, threadCounts, 'home');
-    if (ramCost == -1) {
-        ns.tprint("ERROR Excessive RAM cost, decrease leech %!!!");
-        await ns.sleep(10000);
-        return -1;
+//     let ramCost = getTotalRamNeeded(ns, threadCounts, 'home');
+//     if (ramCost == -1) {
+//         ns.tprint("ERROR Excessive RAM cost, decrease leech %!!!");
+//         await ns.sleep(10000);
+//         return -1;
+//     }
+
+//     let startEndTimes = getStartEndTimes(ns, target, print);
+
+//     ns.printf(`-----------------------------------------------------`);
+//     let batch: HWGW_Job[] = [];
+//     for (const hwgwType of HWGW_TYPES) {
+//         const job = new HWGW_Job(hwgwType, threadCounts[hwgwType+'Threads'], startEndTimes[hwgwType+'Start'], startEndTimes[hwgwType+'End'], ramBlocks[hwgwType+'RamBlock'], 'home');
+//         batch.push(job);
+//     }
+
+//     for (const job of batch) {
+//         ns.exec(HWGW_CONSTANTS[job.hwgw_type].SCRIPT_LOCATION, 'home', {threads: job.threads, temporary: true}, target, job.startTime, job.endTime);
+//     }
+//     ns.printf(`-----------------------------------------------------`);
+
+//     const BUFFER_BATCH_MS = 100;
+    
+//     const assumedMoneyGainPerSecond = ns.getServerMaxMoney(target) * leechPercent / (startEndTimes.weaken2.end * 1000)
+//     const totalRamUsage = ramBlocks.growRamBlock + ramBlocks.weaken1RamBlock + ramBlocks.hackRamBlock + ramBlocks.weaken2RamBlock;
+//     ns.printf(`$/min: ${ns.formatNumber(assumedMoneyGainPerSecond*60)} | RAM: ${ns.formatRam(totalRamUsage)} | Sleeping: ${ns.tFormat(startEndTimes.weaken2.end + BUFFER_BATCH_MS)}`);
+//     await ns.sleep(startEndTimes.weaken2.end + BUFFER_BATCH_MS);
+
+// }
+
+async function exploitImproved(ns: NS, target: string, threads: HWGW_ThreadCounts, subset: ServerSubset, optimalLeechPercent: number, verbose = false) {
+    let startEndTimes = getStartEndTimes(ns, target, verbose);
+    let {maxRamBlock, minRamBlock, ramBlockArray} = calculateRamUsage(threads);
+    const network = new RamNetwork(ns, subset, freeRamPredicate(minRamBlock));
+    const jobs: Job[] = [];
+    for (const [hackType, constants] of Object.entries(HWGW_CONSTANTS)) {
+        jobs.push(new Batch_Job(constants.SCRIPT_LOCATION, threads[hackType], HWGW_TYPE[hackType], target, startEndTimes[hackType].start, startEndTimes[hackType].end))
     }
 
-    let startEndTimes = getStartEndTimes(ns, target, print);
-
-    ns.printf(`-----------------------------------------------------`);
-    let batch: HWGW_Job[] = [];
-    for (const hwgwType of HWGW_TYPES) {
-        const job = new HWGW_Job(hwgwType, threadCounts[hwgwType+'Threads'], startEndTimes[hwgwType+'Start'], startEndTimes[hwgwType+'End'], ramBlocks[hwgwType+'RamBlock'], 'home');
-        batch.push(job);
-    }
-
-    for (const job of batch) {
-        ns.exec(HWGW_CONSTANTS[job.hwgw_type].SCRIPT_LOCATION, 'home', {threads: job.threads, temporary: true}, target, job.startTime, job.endTime);
-    }
-    ns.printf(`-----------------------------------------------------`);
+    network.execNetworkMultiple(jobs, {verbose: verbose});
 
     const BUFFER_BATCH_MS = 100;
-    
-    const assumedMoneyGainPerSecond = ns.getServerMaxMoney(target) * leechPercent / (startEndTimes.weaken2End * 1000)
-    const totalRamUsage = ramBlocks.growRamBlock + ramBlocks.weaken1RamBlock + ramBlocks.hackRamBlock + ramBlocks.weaken2RamBlock;
-    ns.printf(`$/min: ${ns.formatNumber(assumedMoneyGainPerSecond*60)} | RAM: ${ns.formatRam(totalRamUsage)} | Sleeping: ${ns.tFormat(startEndTimes.weaken2End + BUFFER_BATCH_MS)}`);
-    await ns.sleep(startEndTimes.weaken2End + BUFFER_BATCH_MS);
 
+    const assumedMoneyGainPerSecond = ns.getServerMaxMoney(target) * optimalLeechPercent / (startEndTimes.weaken2.end * 1000)
+    ns.printf(`$/min: ${ns.formatNumber(assumedMoneyGainPerSecond*60)} | Sleeping: ${ns.tFormat(startEndTimes.weaken2.end + BUFFER_BATCH_MS)}`);
+
+    await ns.sleep(startEndTimes[HWGW_TYPE.weaken2].end + BUFFER_BATCH_MS);
 }
 
+//For now do the best target at the highest leech% that will fit in ram, will deal with others after formulas
 export async function main(ns: NS) {
     disableLog(ns); ns.tail();
 
-    const target = ns.args[0] as string;
+    const target = ns.args[0] as string || getBestTargetsNaiive(ns, 1, false)[0];
     // const targetCount = ns.args[1] as number || 9;
     const leechPercent = ns.args[1] as number || 0.9;
-    const print = ns.args[2] as boolean || false;
+    const verbose = ns.args[2] as boolean || true;
 
     if (target == 'ALL') {
         const targets = getBestTargetsNaiive(ns, 10, true);
@@ -139,21 +99,36 @@ export async function main(ns: NS) {
         }
     } else {
 
-        if (!isPrepped(ns, target, true)) {
-            await iterativePrep(ns, target);
+        if (!isPrepped(ns, target, verbose)) {
+            await iterativePrepImproved(ns, target);
         }
 
         while (isPrepped(ns, target, true)) {
-            ns.print(`\n`);
-            await exploit(ns, target, leechPercent, print);
+            const subset = ServerSubset.ALL; //TODO: make arg
+            const {optimalLeechPercent, hwgwThreads} = getOptimalLeechPercent(ns, target, subset, true); //TODO: return ServerRamNetwork to pass through directly?
+            ns.print(`\nRunning batch with Optimal Leech %: ${ns.formatPercent(optimalLeechPercent)}\n`);
+            await exploitImproved(ns, target, hwgwThreads, subset, optimalLeechPercent, verbose);
             ns.print(`\n\n----------------`);
         }
 
     }
+
+    // const flags = ns.flags([
+    //     ['targets', "AUTO"],
+    //     ['targetCount', 10],
+    // ]);
+    // if (flags._.length === 0 || flags.help) {
+    //     ns.tprint("This script helps visualize the money and security of a server.");
+    //     ns.tprint(`USAGE: run ${ns.getScriptName()} SERVER_NAME`);
+    //     ns.tprint("Example:");
+    //     ns.tprint(`> run ${ns.getScriptName()} n00dles`)
+    //     return;
+    // }
 }
 
 
 
 export function autocomplete(data, args) {
-    return [...data.servers]; // This script autocompletes the list of servers.
+    // return [...data.servers]; // This script autocompletes the list of servers.
+    return [...data.servers, Object.values(ServerSubset)]
 }
