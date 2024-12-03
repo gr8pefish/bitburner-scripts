a3fdadeba701b1d7908a86f23f1e022368deba34
gr8pefish <gr8pefish@gmail.com>
2024-12-03 09:59:54 -0700

    Update hackUtils
    Add optimalLeech% function, and other small fixes/changes

--- servers/home/hack/hackUtils.ts
+++ servers/home/hack/hackUtils.ts
@@ -1,6 +1,6 @@
 import { HWGW_CONSTANTS } from "../core/constants";
-import { getAllServers } from "../core/coreUtils";
-import { HackTimes } from "../core/datatypes";
+import { freeRamPredicate, getAllServers, RamNetwork } from "../core/coreUtils";
+import { HackTimes, HWGW_StartEndTimes, HWGW_ThreadCounts, ServerSubset } from "../core/datatypes";
 
 /**
  * Gets how long it takes to run a Hack/Grow/Weaken (without formulas)
@@ -11,7 +11,7 @@ import { HackTimes } from "../core/datatypes";
  */
 export function getHgwExecTimes(ns: NS, target: string): HackTimes {
     let hackTime = ns.getHackTime(target);
-    return {HackTime: hackTime, GrowTime: hackTime * HWGW_CONSTANTS.grow.RELATIVE_HACK_TIME, WeakenTime: hackTime * HWGW_CONSTANTS.weaken1.RELATIVE_HACK_TIME}
+    return {hack: hackTime, grow: hackTime * HWGW_CONSTANTS.grow.RELATIVE_HACK_TIME, weaken: hackTime * HWGW_CONSTANTS.weaken1.RELATIVE_HACK_TIME}
 }
 
 // TODO: metric based weighting like this:
@@ -66,5 +66,143 @@ export function getBestTargetsNaiive(ns: NS, listLength: number = 10, print = fa
     return bestServers;
 }
 
+export function getHWGWThreadCounts(ns: NS, target: string, leechPercent: number, print = false): HWGW_ThreadCounts {
+    // hack is simple, just get the leech amt, floor it
+    const hackThreads = Math.floor(ns.hackAnalyzeThreads(target, ns.getServerMaxMoney(target) * leechPercent));
+    
+    // weaken is easy as well, just account for hack increases, ceil just in case
+    const weaken1Threads = Math.ceil((hackThreads * HWGW_CONSTANTS.hack.SEC_CHANGE) / HWGW_CONSTANTS.weaken1.SEC_CHANGE);
+    
+    // grow is trickier - get multiplier from hackThreads vs maxMoney, and ceil it to ensure growth
+    const maxMoney = ns.getServerMaxMoney(target);
+    const multFactor = maxMoney / (maxMoney - (maxMoney * (ns.hackAnalyze(target) * hackThreads)));
+    const growThreads = Math.ceil(ns.growthAnalyze(target, multFactor)); //TODO: add extra to account for leveling?
+    
+    // weaken is easy as well, just account for grow increases, ceil just in case
+    const weaken2Threads = Math.ceil((growThreads * HWGW_CONSTANTS.grow.SEC_CHANGE) / HWGW_CONSTANTS.weaken2.SEC_CHANGE);
+
+    /** Old print statements */
+    // const moneyPrintFrom = ns.getServerMaxMoney(target) - (ns.getServerMaxMoney(target) * leechPercent);
+    // ns.print(`hackThreads: ${hackThreads} for ${ns.formatNumber(ns.getServerMaxMoney(target) * leechPercent)} (${ns.formatPercent(leechPercent)} leech, down to ${ns.formatNumber(moneyPrintFrom)})`);
+    // ns.print(`weaken1Threads: ${weaken1Threads} for ${(hackThreads * HGW_TYPE.HACK.SEC_INCR)} sec incr from hack (${ns.formatNumber(HGW_TYPE.WEAKEN.SEC_DECR)} decr ea)`);
+    // const moneyToPredicted = moneyPrintFrom * multFactor;
+    // ns.print(`growThreads: ${growThreads} for ${multFactor} multFactor, should be from ${ns.formatNumber(moneyPrintFrom)} to ${ns.formatNumber(moneyToPredicted)}(${ns.formatNumber(ns.getServerMaxMoney(target))})`);
+    // ns.print(`weaken2Threads: ${weaken2Threads} for ${(growThreads * HGW_TYPE.GROW.SEC_INCR)} sec incr from grow (${ns.formatNumber(HGW_TYPE.WEAKEN.SEC_DECR)} decr ea)`);
+    // ns.print(`\n`);
+
+    const threadCounts = new HWGW_ThreadCounts(hackThreads, weaken1Threads, growThreads, weaken2Threads);
+    if (print) threadCounts.print(ns);
+
+    return threadCounts;
+}
+
+export function getStartEndTimes(ns: NS, target: string, print = false): HWGW_StartEndTimes {
+    let executionTimes = getHgwExecTimes(ns, target);
+    
+    const BUFFER_MS = 50;
+
+    let startEndTimes = new HWGW_StartEndTimes();
+
+    let longestTime = Math.max(executionTimes.hack, executionTimes.grow, executionTimes.weaken); //assuming weaken for now, will break if wrong
+    let fullTime = longestTime + (BUFFER_MS * 2); //weaken1 (0start) + (grow + weaken2 buffers)
+
+    //weaken2
+    startEndTimes.weaken2.end = fullTime;
+    startEndTimes.weaken2.start = fullTime - executionTimes.weaken;
+
+    //weaken1
+    startEndTimes.weaken1.end = longestTime;
+    startEndTimes.weaken1.start = 0;
+
+    //hack
+    startEndTimes.hack.end = fullTime - BUFFER_MS * 3;
+    startEndTimes.hack.start = startEndTimes.hack.end - executionTimes.hack;
+
+    //grow
+    startEndTimes.grow.end = fullTime - BUFFER_MS;
+    startEndTimes.grow.start = startEndTimes.grow.end - executionTimes.grow;
+
+    //print
+    if (print) startEndTimes.print(ns);
+
+    return startEndTimes
+}
+
+export function getOptimalLeechPercent(ns: NS, preppedTarget: string, subset: ServerSubset, verbose = false): {optimalLeechPercent: number; hwgwThreads: HWGW_ThreadCounts} {
+    let [lowerBound, upperBound] = [0, 1];
+    let optimalPercent = 0; // Tracks the last known working percentage
+    let lastValidThreadCounts: HWGW_ThreadCounts;
+    let lastValidRamUsage: ReturnType<typeof calculateRamUsage>;
+    const ramnet = new RamNetwork(ns, subset);
+    const largestFreeBlock = ramnet.getLargestFreeBlock();
+
+    // Define the minimum interval threshold
+    const threshold = 0.01;
+
+    while (upperBound - lowerBound > threshold) {
+        // Calculate the mid-point for binary search
+        const midPercent = (lowerBound + upperBound) / 2;
+
+        // Calculate thread counts and RAM usage for the current percentage
+        const threadCounts = getHWGWThreadCounts(ns, preppedTarget, midPercent);
+        const ramUsage = calculateRamUsage(threadCounts);
+        const { maxRamBlock, ramBlockArray } = ramUsage;
+
+        // if verbose print debug
+        if (verbose) {
+            ns.print(`\nChecking for optimal percent`);
+            ns.print(`INFO Trying ${ns.formatPercent(midPercent)}`);
+            threadCounts.print(ns);
+            ns.print(`LargestFreeBlock: ${ns.formatRam(largestFreeBlock)} | MaxRamBlock: ${ns.formatRam(maxRamBlock)}`);
+            ns.print(`RamBlockArray Elements:`);
+            ramBlockArray.forEach(ramBlock => ns.print(`${ns.formatRam(ramBlock)}`));
+        }
+
+        if (maxRamBlock > largestFreeBlock) {
+            // The largest block is too large to fit; reduce the upper bound
+            if (verbose) ns.print(`INFO Largest block too big to fit, reducing upper bound`);
+            upperBound = midPercent;
+        } else if (!ramnet.checkRamFit(maxRamBlock, ramBlockArray, verbose)) {
+            // Can't fit all blocks in the RAM network; reduce the upper bound
+            if (verbose) ns.print(`INFO Can't fit all blocks, reducing upper bound`);
+            upperBound = midPercent;
+        } else {
+            // Fits within constraints; record this as the last valid percentage
+            if (verbose) ns.print(`INFO Fits! Attempting to increase`);
+            lowerBound = midPercent;
+            optimalPercent = midPercent; // Update the last valid percent
+            lastValidThreadCounts = threadCounts; // Save thread counts for the last valid percent
+            lastValidRamUsage = ramUsage; // Save RAM usage for the last valid percent
+        }
+    }
+
+    // If verbose, print details of the final valid percentage
+    if (verbose && lastValidThreadCounts && lastValidRamUsage) {
+        ns.print(`\nFinal Optimal Percent: ${ns.formatPercent(optimalPercent)}`);
+        lastValidThreadCounts.print(ns);
+        ns.print(`LargestFreeBlock: ${ns.formatRam(largestFreeBlock)} | MaxRamBlock: ${ns.formatRam(lastValidRamUsage.maxRamBlock)}`);
+    }
+
+    // Return the last known valid percent
+    return {optimalLeechPercent: optimalPercent, hwgwThreads: lastValidThreadCounts};
+}
 
 
+
+export function calculateRamUsage(threadCounts: HWGW_ThreadCounts): { maxRamBlock: number; minRamBlock: number; ramBlockArray: number[] } {
+    // Object.keys(threadCounts).forEach(type => {
+    //     const threadCount = threadCounts[type as keyof HWGW_ThreadCounts];
+    //     const ramCost = threadCount * HWGW_CONSTANTS[type].RAM_COST;
+    // }); //TODO
+    const ramBlockArray = [
+        threadCounts.hack * HWGW_CONSTANTS.hack.RAM_COST,
+        threadCounts.weaken1 * HWGW_CONSTANTS.weaken1.RAM_COST,
+        threadCounts.grow * HWGW_CONSTANTS.grow.RAM_COST,
+        threadCounts.weaken2 * HWGW_CONSTANTS.weaken2.RAM_COST,
+    ];
+    
+    const maxRamBlock = Math.max(...ramBlockArray);
+    const minRamBlock = Math.min(...ramBlockArray);
+    
+    return { maxRamBlock, minRamBlock, ramBlockArray };
+}
